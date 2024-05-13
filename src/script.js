6553ecdce12f613f28d00e9888e9b3a876538ec2
Tobyrams@gmail.com <Tobyrams@gmail.com>
2024-05-13 23:54:28 +0200

    Dashboard v.0.1
    Added to cards on the dashboard and also made them scrollable on drag

--- src/script.js
+++ src/script.js
@@ -36,3 +36,47 @@ document.addEventListener("DOMContentLoaded", function () {
   // Update the content of the element with id "currentDate"
   document.getElementById("currentDate").textContent = formattedDate;
 });
+
+// ---------------------Card Horizontal  Scroll------------------
+// https://www.youtube.com/watch?v=C9EWifQ5xqA
+
+// Select the element with the class "sml__cards" and assign it to the variable "slider"
+const slider = document.querySelector(".sml__cards");
+
+// Initialize variables for tracking mouse events and scroll position
+let isDown = false;
+let startX;
+let scrollLeft;
+
+// Event listener for mouse button down event on the slider
+slider.addEventListener("mousedown", (e) => {
+  // Set "isDown" flag to true to indicate mouse button is down
+  isDown = true;
+  // Calculate the initial position of the mouse pointer relative to the slider
+  startX = e.pageX - slider.offsetLeft;
+  // Store the initial scroll position of the slider
+  scrollLeft = slider.scrollLeft;
+});
+// Event listener for mouse leaving the slider area
+slider.addEventListener("mouseleave", () => {
+  // Reset the "isDown" flag to false when mouse leaves the slider area
+  isDown = false;
+});
+// Event listener for mouse button up event on the slider
+slider.addEventListener("mouseup", () => {
+  // Reset the "isDown" flag to false when mouse button is released
+  isDown = false;
+});
+// Event listener for mouse movement over the slider
+slider.addEventListener("mousemove", (e) => {
+  // Check if mouse button is not down, exit the function
+  if (!isDown) return;
+  // Prevent default behavior of mousemove event (e.g., text selection)
+  e.preventDefault();
+  // Calculate the new position of the mouse pointer relative to the slider
+  const x = e.pageX - slider.offsetLeft;
+  // Calculate the distance moved by the mouse pointer from the initial position
+  const walk = x - startX;
+  // Update the scroll position of the slider by subtracting the distance moved
+  slider.scrollLeft = scrollLeft - walk;
+});
